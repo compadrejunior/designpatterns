27d8841aef56cf60bdc1ff45c3d13cee9e3d3605
José Compadre Junior <jose.compadre@oatsolution.com.br>
2021-04-12 15:55:58 -0300

    Added Singleton pattern

---
+++ src/main/java/br/com/oatsolutions/gof/Singleton.java
@@ -0,0 +1,28 @@
+package br.com.oatsolutions.gof;
+
+public class Singleton {
+    /**
+     * The uniqueInstance private member grants control over the class
+     */
+    private static Singleton uniqueInstance;
+
+    /**
+     * The private constructor prevents the object being instantiated from
+     * another object
+     */
+    private Singleton(){}
+
+    /**
+     * The getInstance method is the pattern you will see in every Singleton
+     * pattern classes
+     * @return an unique instance of this class no matter how many time it
+     * is called
+     */
+    public static Singleton getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new Singleton();
+        }
+        return  uniqueInstance;
+    }
+}
+
